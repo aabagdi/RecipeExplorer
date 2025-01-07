5f90ca0e56287e3e75d44a0c8ecf60ebeab20422
Aadit Bagdi <70984836+aabagdi@users.noreply.github.com>
2025-01-07 13:35:10 -0800

    Update AsyncImageCached.swift

--- RecipeExplorer/Caching/AsyncImageCached.swift
+++ RecipeExplorer/Caching/AsyncImageCached.swift
@@ -9,10 +9,10 @@ import Foundation
 import SwiftUI
 
 struct AsyncImageCached<Content> : View where Content : View {
-    internal let url : URL
-    internal let scale : CGFloat
-    internal let transaction : Transaction
-    internal let content : (AsyncImagePhase) -> Content
+    internal let url: URL
+    internal let scale: CGFloat
+    internal let transaction: Transaction
+    internal let content: (AsyncImagePhase) -> Content
     
     init(url: URL,
          scale: CGFloat = 1.0,
